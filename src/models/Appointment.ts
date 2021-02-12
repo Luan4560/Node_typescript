bf4cadc42d0e44f5c4be1e7880fda872ff4871dc
Luan Nascimento <luan.nascimento11@outlook.com>
2021-02-12 01:22:15 -0300

    Models concept

---
+++ src/models/Appointment.ts
@@ -0,0 +1,18 @@
+import { isThisSecond } from 'date-fns';
+import { v4 } from 'uuid';
+
+class Appointment {
+  id: string;
+
+  provider: string;
+
+  date: Date;
+
+  constructor(provider: string, date: Date) {
+    this.id = v4();
+    this.provider = provider;
+    this.date = date;
+  }
+}
+
+export default Appointment;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,8 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const users = {
-    name,
-    email,
-  };
-
-  return response.json(users);
-});
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
